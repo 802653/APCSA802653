cb90b249682f8270cf9a201e07a12095f4b2f944
802653 <54371363+802653@users.noreply.github.com>
2019-09-20 10:02:22 -0700

    te
    rw

---
+++ SentenceBuilder/SentenceBuilder.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class SentenceBuilder here.
+ *
+ * @author (Pblakenberg)
+ * @version (92019)
+ */
+public class SentenceBuilder
+{
+    String[] nouns = {"puppy","pizza", "snake","boy","girl","baby","doorbell","building", "mortal"};
+    String[] verbs = {"jumps", "runs", "flies", "eats", "makes", "cooks", "swims", "wants"};
+    String[] adjectives = {"little", "silly", "funny", "stuffy", "shady", "hopeful", "graceful"};
+    String[] determiners = {"the", "my", "your", "his","hers","our","their"};
+    String[] adverbs = {"quickly", "happily", "smoothly", "often", "very", "never"};
+    String[] prepositions = {"over","near","on top of","behind","next to","inside of", "under"};
+    private String sentence = "";
+    public SentenceBuilder() 
+    {
+        
+    }
+    public String returnSentence() {
+        return sentence;
+    }
+    public String buildNoun
+}
